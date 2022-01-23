fa57b2d292b3c91a41b3baa68b913f6657492743
Morgan Lefranc <mlefranc@ridge-i.com>
2022-01-23 16:50:46 +0900

    Implemented the row removing logic

--- day_3/d3.py
+++ day_3/d3.py
@@ -1,3 +1,8 @@
+"""
+Advent of Code 2021 - Day 3
+Problem link: https://adventofcode.com/2021/day/3
+"""
+
 from pathlib import Path
 
 import numpy as np
@@ -7,21 +12,40 @@ INPUT_PATH = Path("day_3/input.txt")
 
 def calculate_gamma(arr: np.ndarray) -> int:
     majority = calculate_majority_value_in_cols(arr)
-    majority = majority.astype(str).tolist()
-    return int("".join(majority), base=2)
+    return _convert_bin_array_to_int(majority)
 
 
-def calculate_majority_value_in_cols(arr):
-    nb_lines = len(arr)
-    col_sums = arr.sum(axis=0) / nb_lines
-    majority = np.floor(col_sums + 0.5).astype(int)
+def _convert_bin_array_to_int(bin_array: np.ndarray) -> int:
+    bin_array = bin_array.astype(str).tolist()
+    return int("".join(bin_array), base=2)
+
+
+def calculate_majority_value_in_cols(
+    arr: np.ndarray, round_up: bool = True
+) -> np.ndarray:
+    """For each column of the input binary array, computes the majority value (0 or 1).
+    In case of equal number of occurrences of 0 and 1 in a column,
+    returns float(round_up).
+
+    Args:
+        arr (np.ndarray): Binary array (all values are either 0 or 1)
+        round_up (bool, optional): Whether to return 0 or 1 for a column in case of
+        equal occurrences of 0 and 1. Defaults to True.
+
+    Returns:
+        np.ndarray: An array of shape (1, C), where C is the number of columns in arr.
+        Each value describes the majority value in each of the columns of arr.
+    """
+    col_sums = np.mean(arr, axis=0)
+    # Converts boolean to +- 0.01
+    round_factor = ((float(round_up) * 2) - 1) / 100
+    majority = np.floor(col_sums + 0.5 + round_factor).astype(int)
     return majority
 
 
 def calculate_epsilon(arr: np.ndarray) -> int:
     minority = 1 - calculate_majority_value_in_cols(arr)
-    minority = minority.astype(str).tolist()
-    return int("".join(minority), base=2)
+    return _convert_bin_array_to_int(minority)
 
 
 def read_file(pth: Path) -> np.ndarray:
@@ -35,7 +59,42 @@ def read_file(pth: Path) -> np.ndarray:
     return arr
 
 
-def get_power_consumption(arr: np.ndarray) -> int:
+def get_oxygen_generator_rating(arr: np.ndarray) -> int:
+    ...
+
+
+def get_co2_scrubber_rating(arr: np.ndarray) -> int:
+    ...
+
+
+def remove_non_compliant_rows(
+    arr: np.ndarray, col_nb: int, round_up: bool
+) -> np.ndarray:
+    """Remove rows from the input array that do not have the majority value in their
+    `col_nb`th column.
+
+    Args:
+        arr (np.ndarray): Input binary array of shape (N, M)
+        col_nb (int): Column to use to consider whether the row should be removed.
+            Must be < M.
+        round_up (bool): Decides on which value should be considered majority in case of
+            equality (0 or 1).
+
+    Returns:
+        np.ndarray: Binary array of shape (<= N, M).
+    """
+    cols_majority = calculate_majority_value_in_cols(arr, round_up=round_up)
+    rows_to_delete = []
+    for row, val in enumerate(arr[:, col_nb]):
+        if val != cols_majority[col_nb]:
+            rows_to_delete.append(row)
+
+    arr = np.delete(arr, rows_to_delete, 0)
+
+    return arr
+
+
+def p1_get_power_consumption(arr: np.ndarray) -> int:
     gamma = calculate_gamma(arr)
     epsilon = calculate_epsilon(arr)
     return gamma * epsilon
@@ -43,4 +102,4 @@ def get_power_consumption(arr: np.ndarray) -> int:
 
 if __name__ == "__main__":
     arr = read_file(INPUT_PATH)
-    print("Power consumption: ", get_power_consumption(arr))
+    print("Power consumption: ", p1_get_power_consumption(arr))
--- tests/day_3/test_day3.py
+++ tests/day_3/test_day3.py
@@ -59,3 +59,91 @@ def test_read_gamma(placeholder_data: np.ndarray):
 def test_read_epsilon(placeholder_data: np.ndarray):
     epsilon = d3.calculate_epsilon(placeholder_data)
     assert epsilon == 9
+
+
+def test_get_oxygen_generator_rating(placeholder_data: np.ndarray):
+    oxygen_generator_rating = d3.get_oxygen_generator_rating(placeholder_data)
+    assert oxygen_generator_rating == 23
+
+
+def test_get_co2_scrubber_rating(placeholder_data: np.ndarray):
+    co2_scrubber_rating = d3.get_co2_scrubber_rating(placeholder_data)
+    assert co2_scrubber_rating == 10
+
+
+def test_majority_0():
+    data = np.array(
+        [
+            [0, 0, 1],
+            [1, 1, 0],
+            [0, 0, 0],
+        ]
+    )
+    majority = d3.calculate_majority_value_in_cols(data)
+    assert np.array_equal(majority, [0, 0, 0])
+
+
+def test_majority_1():
+    data = np.array(
+        [
+            [1, 0, 1],
+            [1, 1, 0],
+            [0, 1, 1],
+        ]
+    )
+    majority = d3.calculate_majority_value_in_cols(data)
+    assert np.array_equal(majority, [1, 1, 1])
+
+
+def test_ambiguous_majority_1():
+    data = np.array(
+        [
+            [0, 0, 1],
+            [1, 1, 0],
+            [0, 1, 1],
+            [1, 0, 0],
+        ]
+    )
+    majority = d3.calculate_majority_value_in_cols(data, round_up=True)
+    assert np.array_equal(majority, [1, 1, 1])
+
+
+def test_ambiguous_majority_0():
+    data = np.array(
+        [
+            [0, 0, 1],
+            [1, 1, 0],
+            [0, 1, 1],
+            [1, 0, 0],
+        ]
+    )
+    majority = d3.calculate_majority_value_in_cols(data, round_up=False)
+    assert np.array_equal(majority, [0, 0, 0])
+
+
+def test_remove_2_rows_1st_position():
+    data = np.array(
+        [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 1],
+            [1, 0, 1],
+        ]
+    )
+    filtered_arr = d3.remove_non_compliant_rows(data, 0, round_up=False)
+    assert filtered_arr.shape == (2, 3)
+    assert np.array_equal(filtered_arr, np.array([[0, 1, 0], [0, 0, 1]]))
+
+
+def test_remove_2_rows_2nd_position():
+    data = np.array(
+        [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 1],
+            [1, 0, 1],
+        ]
+    )
+    filtered_arr = d3.remove_non_compliant_rows(data, 1, round_up=False)
+    assert filtered_arr.shape == (2, 3)
+    assert np.array_equal(filtered_arr, np.array([[0, 0, 1], [1, 0, 1]]))
